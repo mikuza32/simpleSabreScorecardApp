216c731ac54691f8c6daec0daa36578633c5e656
ZaneMikula <120997447+mikuza32@users.noreply.github.com>
2024-11-14 01:46:11 -0500

    Delete simpleSabreScorecardApp-master directory

--- simpleSabreScorecardApp-master/src/main/java/com/github/mikuza32/simplesabrescorecardapp/Entities/countingStatistics.java
+++
@@ -1,4 +0,0 @@
-package com.github.mikuza32.simplesabrescorecardapp.Entities;
-
-public class countingStatistics {
-}
--- simpleSabreScorecardApp-master/src/main/java/com/github/mikuza32/simplesabrescorecardapp/Repo/offensiveSabermetricRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.mikuza32.simplesabrescorecardapp.Repo;
-import  com.github.mikuza32.simplesabrescorecardapp.Entities.offensiveSabermetrics;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface offensiveSabermetricRepository extends JpaRepository<offensiveSabermetrics, Long> {
-
-}
